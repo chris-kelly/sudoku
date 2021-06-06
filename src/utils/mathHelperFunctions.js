97ff23dbd11ad46a7f4df2fa1006d8daca2fec4a
chris-kelly <chris_kelly@live.co.uk>
2021-06-06 13:37:08 +0100

    Added integer to new base conversion functions

--- src/utils/mathHelperFunctions.js
+++ src/utils/mathHelperFunctions.js
@@ -1,6 +1,6 @@
 // FUNCTIONS TO HELP WITH MATHS
 
-import { range, floor, random } from 'mathjs'
+import { range, floor, random, pow } from 'mathjs'
 // const mathjs = require('mathjs'); const floor = mathjs.floor; const random = mathjs.random; const subset = mathjs.subset; const index = mathjs.index
 
 /**
@@ -69,7 +69,7 @@ const recursive_combinations = function(array) {
 
 /**
  * Generate n choose k ordered combinations from an array
- * @param {object} x - Aray to generate combinations from
+ * @param {object} x - Array to generate combinations from
  * @param {object} k - Number of items to select from
  */
 const generate_n_choose_k_combination = function(x,k) {
@@ -91,10 +91,42 @@ const generate_n_choose_k_combination = function(x,k) {
     return(permArr)
 }
 
+/**
+ * Convert number from base x to base 10
+ * @param {number} x - Integer to convert
+ * @param {number} old_base - Original base of integer
+ */
+const convert_base_x_to_10 = function(x,old_base) {
+    var z = x.toString().split('')
+    var z = z.map((v,i) => parseInt(v)*pow(old_base,z.length-i-1))
+    var result = 0
+    for(i = 0; i < z.length; i++) {
+      var result = result + z[i]
+    }
+    return(result)
+}
+
+/**
+ * Convert number from base x to base 10
+ * @param {number} x - Integer to convert
+ * @param {number} new_base - New base for integer
+ */
+const convert_base_10_to_x = function(x,new_base) {
+    var remainder = x % new_base
+    var big = (x - remainder)/new_base
+    if(big <= 6) {
+      return(parseInt(big.toString().concat(remainder)))
+    } else {
+      return(parseInt(base_10_to_x(big,new_base).toString().concat(remainder)))
+    }
+}
+
 export {
     isItemInArray as isItemInArray,
     gRange as gRange,
     shuffleArray as shuffleArray,
     recursive_combinations as recursive_combinations,
     generate_n_choose_k_combination as generate_n_choose_k_combination,
+    convert_base_x_to_10 as convert_base_x_to_10,
+    convert_base_10_to_x as convert_base_10_to_x,
 }
