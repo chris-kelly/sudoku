b836d5cf2b921c84f2ac2b3d7c2f13bbad1b78e2
chris-kelly <chris_kelly@live.co.uk>
2023-01-08 18:51:25 +0000

    Refactor - move game to own file; simplify styles

--- src/utils/renderHelperFunctions.js
+++ src/utils/renderHelperFunctions.js
@@ -7,9 +7,41 @@ import { gRange } from './mathHelperFunctions'
  * Check if index is divisible by 3 (to make every 3rd border thicker in sudoku grid)
  * @param {number} n 
  */
-const checkIndexDivisibleThree = (n) => {
+export const checkIndexDivisibleThree = (n) => {
     return(n % 3 === 0)
 };
+
+export const cellViewStyle = function(props, defaultStyle = {borderWidth: 0.5}) {
+  var borderWidths = {
+    // Make the top border of the selected cell very thick (4). Otherwise, make the top border of rows 0,3,6 medium thick (2). Otherwise, thin (0.5).
+    borderTopWidth: props.isSelectedCell ? 4 : checkIndexDivisibleThree(props.i) ? 2 : 0.5,
+    // Make the left border of the selected cell very thick (4). Otherwise, make the left border of columns 0,3,6 medium thick (2). Otherwise, thin (0.5).
+    borderLeftWidth: props.isSelectedCell ? 4 : checkIndexDivisibleThree(props.j) ? 2 : 0.5,
+    // Make the bottom border of the selected cell very thick (4). Otherwise, make the bottom border of last row 8 medium thick (2). Otherwise, thin (0.5).
+    borderBottomWidth: props.isSelectedCell ? 4 : props.i === 8 ? 2 : 0.5,
+    // Make the right border of the selected cell very thick (4). Otherwise, make the right border of last column 8 medium thick (2). Otherwise, thin (0.5).
+    borderRightWidth: props.isSelectedCell ? 4 : props.j === 8 ? 2 : 0.5,
+  }
+  var cellColors = {
+    backgroundColor : props.backgroundColor,
+    borderColor : props.borderColor,
+  }
+  return([defaultStyle,cellColors,borderWidths])
+}
+
+export const cellTextStyle = function(props, defaultStyle = {fontSize: 20}) {
+  // Text colour depends on if it is user input or not 
+  return([
+    {color: this.props.textColor},
+    defaultStyle,
+  ])
+}
+
+export const numberValueRender = function(rValue) {
+  // If value is zero, render cell text as blank
+  var valueRender = rValue === 0 ? null : rValue;
+  return(valueRender)
+}
 
 // 
 /**
@@ -19,7 +51,7 @@ const checkIndexDivisibleThree = (n) => {
  * @param {object} s_dict 
  * @param {boolean} showPotentialGrid 
  */
-const megaSudokuInput = function(preFilled,s_dict,showPotentialGrid=false) {
+ export const megaSudokuInput = function(preFilled,s_dict,showPotentialGrid=false) {
     var megaGrid = preFilled // pre-filled are just scalars
     var s_matrix = s_dict['sudoku']._data
     for(let i = 0; i < 9; i++) {
@@ -33,10 +65,3 @@ const megaSudokuInput = function(preFilled,s_dict,showPotentialGrid=false) {
     }
     return(megaGrid)
 };
-
-
-export {
-    megaSudokuInput as megaSudokuInput,
-    checkIndexDivisibleThree as checkIndexDivisibleThree,
-}
-
